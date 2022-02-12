45ddee312a04eca08d60b8945089fd0eb2efedc1
Olgamalyuta <89601793+Olgamalyuta@users.noreply.github.com>
2022-02-12 11:57:47 +0300

    Delete Task2 directory

--- Task2/ShowResult.java
+++
@@ -1,7 +0,0 @@
-package by.epam.Kovalenko.Task2;
-
-public class ShowResult {
-    public static void showResult(double perim, double square){
-        System.out.println("Длина периметра- " + perim + " Площадь треугольника -" + square);
-    }
-}
